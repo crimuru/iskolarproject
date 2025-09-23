89f0e1c8af35d9aebedc9cf449c66d5c31b5f958
LAB23_USER ON WS12 <L23Y15W12@LabHCI.net>
2025-09-23 17:23:00 +0800

    setup(loginpage): create page

--- iSKOLAR/iSKOLARapp/views.py
+++ iSKOLAR/iSKOLARapp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def iSKOLARapp_view(request):
     return render(request, "iskolar.html")
+def login_view(request):
+    return render(request, "login.html")
